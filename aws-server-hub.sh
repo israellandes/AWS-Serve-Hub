c4a804cf892580813ea082b92777a8b9c644065b
root <root@ip-172-31-17-50.us-west-1.compute.internal>
2022-09-04 22:06:51 +0000

    go

--- aws-server-hub.sh
+++ aws-server-hub.sh
@@ -13,3 +13,12 @@ cat ~/.aws/config
 cat ~/.aws/credentials
 }
 
+download_bucket()
+{
+yourbucket=backups-p2
+youramount=10
+yourbucket=$1
+youramount=$2
+aws s3api list-objects --bucket $yourbucket --max-items $youramount
+}
+download_bucket $1 $2
